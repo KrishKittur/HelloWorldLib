6e14cd39c73ec2bea0b3ce43156fcfadb0f1f924
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2024-09-15 01:40:20 -0400

    remove stuff from default.nix

--- default.nix
+++ default.nix
@@ -5,22 +5,4 @@ stdenv.mkDerivation rec {
     version = "0.1.0";
     src = ./.;
     nativeBuildInputs = [ cmake ];
-
-    preConfigure = ''
-    rm -rf build CMakeCache.txt
-    mkdir -p build
-    cd build
-  '';
-
-  configurePhase = ''
-    cmake ${src} .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$out
-  '';
-
-  buildPhase = ''
-    cmake --build .
-  '';
-
-  installPhase = ''
-    cmake --install .
-  '';
 }
